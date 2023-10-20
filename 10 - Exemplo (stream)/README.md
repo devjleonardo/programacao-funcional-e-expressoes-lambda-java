74cb18d1a5a03aec7b539551d6894ba7088c7374
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-10-20 15:23:15 -0300

    Exemplo (stream)

---
+++ 10 - Exemplo (stream)/README.md
@@ -0,0 +1,5 @@
+## Exemplo utilizando - filter, sorted, map, reduce
+![image](https://raw.githubusercontent.com/devjleonardo/assets/main/programacao-funcional-e-expressoes-lambda-java/09_Exemplo%20(stream)/exemplo.png)
+
+### Exemplo do programa rodando
+![image](https://raw.githubusercontent.com/devjleonardo/assets/main/programacao-funcional-e-expressoes-lambda-java/09_Exemplo%20(stream)/exemplo%20do%20programa%20rodando.png)
---
+++ 10 - Exemplo (stream)/src/application/Program.java
@@ -0,0 +1,59 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import entites.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Enter full file path: ");
+		String path = scanner.nextLine();
+		
+		try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
+			List<Product> list = new ArrayList<>();
+			
+			String line = bufferedReader.readLine();
+			
+			while (line != null) {
+				String[] fields = line.split(",");
+				
+				list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+				
+				line = bufferedReader.readLine();
+			}
+			
+			double avg = list.stream()
+					.map(p -> p.getPrice())
+					.reduce(0.0, (x, y) -> x + y) / list.size();
+			
+			System.out.println("Average price: " + String.format("%.2f", avg));
+			
+			Comparator<String> comparator = (string1, string2) -> string1.toUpperCase().compareTo(string2.toUpperCase());
+			
+			List<String> names = list.stream()
+					.filter(p -> p.getPrice() < avg)
+					.map(p -> p.getName())
+					.sorted(comparator.reversed())
+					.collect(Collectors.toList());
+			
+			names.forEach(System.out::println);
+			
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		
+		scanner.close();
+	}
+	
+}
